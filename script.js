f3ea3141ee11c511a2587287cea99e62af13810a
arunps9645-source <arunps9645@gmail.com>
2026-06-10 10:18:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+const password = document.getElementById("password");
+const strengthBar = document.getElementById("strength");
+const result = document.getElementById("result");
+const tips = document.getElementById("tips");
+
+password.addEventListener("input", () => {
+
+    let score = 0;
+    let suggestions = [];
+
+    const pwd = password.value;
+
+    if (pwd.length >= 8) {
+        score += 20;
+    } else {
+        suggestions.push("Use at least 8 characters");
+    }
+
+    if (/[A-Z]/.test(pwd)) {
+        score += 20;
+    } else {
+        suggestions.push("Add uppercase letters");
+    }
+
+    if (/[a-z]/.test(pwd)) {
+        score += 20;
+    } else {
+        suggestions.push("Add lowercase letters");
+    }
+
+    if (/[0-9]/.test(pwd)) {
+        score += 20;
+    } else {
+        suggestions.push("Add numbers");
+    }
+
+    if (/[^A-Za-z0-9]/.test(pwd)) {
+        score += 20;
+    } else {
+        suggestions.push("Add special characters");
+    }
+
+    strengthBar.style.width = score + "%";
+
+    if (score <= 40) {
+        strengthBar.style.background = "red";
+        result.innerText = "Weak Password";
+    } else if (score <= 80) {
+        strengthBar.style.background = "orange";
+        result.innerText = "Medium Password";
+    } else {
+        strengthBar.style.background = "green";
+        result.innerText = "Strong Password";
+    }
+
+    tips.innerHTML = "";
+
+    suggestions.forEach(item => {
+        let li = document.createElement("li");
+        li.textContent = item;
+        tips.appendChild(li);
+    });
+});
